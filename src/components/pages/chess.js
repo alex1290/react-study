5857c3cac557d0f0c972a76e4c21fd8a98f284f7
Alex Cheng/WHQ/Wistron <Alex_Cheng@wistron.com>
2019-11-19 16:17:26 +0800

    chess B

--- src/components/pages/chess.js
+++ src/components/pages/chess.js
@@ -6,55 +6,201 @@ import * as action from '../../action/index'
 class Game extends React.Component {
     state = {
         selected: null,
-        rule:{
-            P:[[0,1],[0,2]],
-            R:[[1,0],[0,1]],
-            K:[[1,2],[2,1]],
-            B:[[1,1]],
-            Q:[[1,0],[0,1],[1,1]],
-            K:[[1,0],[0,1],[1,1]],
+        rule: {
+            P: [[0, 1], [0, 2]],
+            R: [[1, 0], [0, 1]],
+            K: [[1, 2], [2, 1]],
+            B: [[1, 1]],
+            Q: [[1, 0], [0, 1], [1, 1]],
+            K: [[1, 0], [0, 1], [1, 1]],
         }
     }
+
     selectPiece = (position) => {
+        let square = (x, y) => {
+            if (x < 8 && y < 8 && x >= 0 && y >= 0) {
+                let sq = document.getElementsByClassName('square')[y * 8 + x].innerHTML
+                console.log(sq)
+                return sq ? sq : 'Null'
+            }
+            return 'Out'
+        }
         let piece = document.getElementsByClassName('square')[position]
-        if (piece.innerHTML && piece.innerHTML[0] === (this.props.chessState.blackIsNext ? 'B' : 'W')) {
+        let color = piece.innerHTML[0]
+        let name = piece.innerHTML.substring(1)
+        if (color === (this.props.chessState.blackIsNext ? 'B' : 'W')) {
             document.querySelectorAll('.square').forEach((i) => i.style.borderColor = '#000')
-            piece.style.borderColor = '#f00'
-            let row = Math.floor(position / 8)
-            let col = position % 8
+
+            let x = position % 8
+            let y = Math.floor(position / 8)
+            let col = [0, 1, 2, 3, 4, 5, 6, 7]
+            let row = [0, 1, 2, 3, 4, 5, 6, 7]
+            let movement = []
+            switch (name) {
+                case 'P':
+                    if (color === 'B') {
+                        if (square(x, y - 1) === 'Null') {
+                            movement.push([x, y - 1])
+                            if (y === 6 && square(x, y - 2) === 'Null') {
+                                movement.push([x, y - 2])
+                            }
+                        }
+
+                        if (square(x + 1, y - 1) && square(x + 1, y - 1)[0] !== color && square(x + 1, y - 1) !== 'Null' && square(x + 1, y - 1) !== 'Out') {
+                            movement.push([x + 1, y - 1])
+                        }
+                        if (square(x - 1, y - 1) && square(x - 1, y - 1)[0] !== color && square(x - 1, y - 1) !== 'Null' && square(x - 1, y - 1) !== 'Out') {
+                            movement.push([x - 1, y - 1])
+                        }
+                    } else {
+                        if (square(x, y + 1) === 'Null') {
+                            movement.push([x, y + 1])
+                            if (y === 1 && square(x, y + 2) === 'Null') {
+                                movement.push([x, y + 2])
+                            }
+                        }
+
+                        if (square(x + 1, y + 1) && square(x + 1, y + 1)[0] !== color && square(x + 1, y + 1) !== 'Null' && square(x + 1, y + 1) !== 'Out') {
+                            movement.push([x + 1, y + 1])
+                        }
+                        if (square(x - 1, y + 1) && square(x - 1, y + 1)[0] !== color && square(x - 1, y + 1) !== 'Null' && square(x - 1, y + 1) !== 'Out') {
+                            movement.push([x - 1, y + 1])
+                        }
+                    }
+                    break;
+                case 'R':
+                    for (let i = x + 1; i < 8; i++) {
+                        if (square(i, y)[0] !== color)
+                            movement.push([i, y])
+                        if (square(i, y) !== 'Null') {
+                            break
+                        }
+                    }
+                    for (let i = x - 1; i >= 0; i--) {
+                        console.log(square(i, y)[0])
+                        if (square(i, y)[0] !== color)
+                            movement.push([i, y])
+                        if (square(i, y) !== 'Null') {
+                            break
+                        }
+                    }
+                    for (let i = y + 1; i < 8; i++) {
+                        if (square(x, i)[0] !== color)
+                            movement.push([x, i])
+                        if (square(x, i) !== 'Null') {
+                            break
+                        }
+                    }
+                    for (let i = y - 1; i >= 0; i--) {
+                        if (square(x, i)[0] !== color)
+                            movement.push([x, i])
+                        if (square(x, i) !== 'Null') {
+                            break
+                        }
+                    }
+                    break;
+                case 'K':
+                    let knight = [[x + 1, y + 2], [x - 1, y - 2], [x + 1, y - 2], [x - 1, y + 2], [x - 2, y - 1], [x + 2, y - 1], [x - 2, y + 1], [x + 2, y + 1]]
+                    knight.forEach(([x, y]) => {
+                        console.log(x, y, square(x, y))
+                        if (square(x, y) !== 'Out' && square(x, y)[0] !== color) {
+                            movement.push([x, y])
+                        }
+                    })
+                    break;
+                case 'B':
+                    for (let i = x + 1; i < 8; i++) {
+                        if (square(i, y)[0] !== color)
+                            movement.push([i, y])
+                        if (square(i, y) !== 'Null') {
+                            break
+                        }
+                    }
+                // col.map((i) => {
+                //     if(i!==x && square(i,y)[0] !== color)
+                //     movement.push([i, y])
+                // })
+                // row.map((i) => {
+                //     if(i!==y && square(x,i)[0] !== color)
+                //     movement.push([x, i])
+                // })
+
+            }
+            if (movement) {
+                movement.map(([x, y]) => {
+                    document.getElementsByClassName('square')[y * 8 + x].style.borderColor = '#f00'
+                })
+            }
+            piece.style.borderColor = '#0c0'
             this.setState({
                 selected: {
                     name: piece.innerHTML,
-                    position: [row, col]
+                    position: [x, y]
                 }
             })
         }
     }
+
     movePiece = (newPosition) => {
         let { history, stepNumber } = this.props.chessState
         let newHistory = [...history[stepNumber]]
         let { name, position } = this.state.selected
-        let [row, col] = position
+        let [x, y] = position
         let newRow = Math.floor(newPosition / 8)
         let newCol = newPosition % 8
+        let target = document.getElementsByClassName('square')[newPosition].innerHTML
         let color = name[0]
         let piece = name.substring(1)
+        let choose = document.getElementsByClassName('square')[newPosition]
         const move = () => {
             newHistory[newPosition] = name;
-            newHistory[row * 8 + col] = null;
+            newHistory[y * 8 + x] = null;
             document.querySelectorAll('.square').forEach((i) => i.style.borderColor = '#000')
             this.setState({ selected: null });
             this.props.dispatch(action.moveChess(newHistory));
         }
-        console.log(newRow)
-        switch (piece) {
-            case 'P':
-                if (color === 'B' && newRow === (row - 1 || row - 2) && newCol === col) {
-                   move()
-                }else if(color === 'W' && newRow === (row + 1 || row + 2) && newCol === col){
-                    move()
-                }
+
+        const cancel = () => {
+            document.querySelectorAll('.square').forEach((i) => i.style.borderColor = '#000')
+            this.setState({ selected: null });
         }
+
+
+        if (choose.style.borderColor == 'rgb(255, 0, 0)') {
+            move()
+        } else {
+            cancel()
+        }
+        // const isExist = (row, col, newRow, newCol, color) => {
+        //      if(row === newRow ){
+        //         let between = []
+        //         for(let i = col;i < newCol - col )
+        //      }
+        // }
+        // rule
+        // switch (piece) {
+        //     case 'P':
+        //         if (color === 'B') {
+        //             if (!target && (newRow === row - 1 || (row === 6 && newRow === 4)) && newCol === col) {
+        //                 move()
+        //             } else if (target && target[0] !== color && newRow === row - 1 && (newCol === col + 1 || newCol === col - 1)) {
+        //                 move()
+        //             }
+        //         } else if (color === 'W') {
+        //             if (!target && (newRow === row + 1 || (row === 1 && newRow === 3)) && newCol === col) {
+        //                 move()
+        //             } else if (target && target[0] !== color && newRow === row + 1 && (newCol === col + 1 || newCol === col - 1)) {
+        //                 move()
+        //             }
+        //         }
+        //         break;
+        //     case 'R':
+        //         console.log(piece, 'R')
+        //         if (target[0] !== color && (newCol === col || newRow === row)) {
+        //             move()
+        //         }
+        //         break;
+        // }
 
 
     }
@@ -86,7 +232,9 @@ class Game extends React.Component {
                     <h2>{blackIsNext ? 'Black' : 'White'} turn</h2>
                     <ul className="stepBoard">
                         {history.map((item, index) =>
-                            <li className="step">step: {index + 1}</li>
+                            <li className="step"
+                                key={index}
+                            >step: {index + 1}</li>
                         )}
                     </ul>
                 </div>
