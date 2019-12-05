852699c601a311d37ddd5e1594588c2df62e2507
Alex Cheng/WHQ/Wistron <Alex_Cheng@wistron.com>
2019-12-05 11:37:10 +0800

    add

--- src/components/pages/crawler.js
+++ src/components/pages/crawler.js
@@ -12,12 +12,23 @@ const ptt = () => {
         const $ = cheerio.load(body);
         const result = [];
         const table_tr = $(".r-ent");
-        
-        for (let i = 0; i < table_tr.length; i++) { 
+
+        for (let i = 0; i < table_tr.length; i++) {
             const table_td = table_tr.eq(i);
             const title = table_td.find('.title').text();
-            console.log(time);
+            const push = table_td.find('.nrec').text();
+            const author = table_td.find('.author').text();
+            const date = table_td.find('.date').text();
+
+            const link = title.indexOf('(本文已被刪除)') === -1
+                ? table_td.find('a').attr('href')
+                : '';
+            const item = { title, link, push, author, date }
+            result.push(item)
         }
+        console.log(result);
+        fs.writeFileSync("result.json", JSON.stringify(result));
     })
 }
 ptt()
+setInterval(ptt,1000);
